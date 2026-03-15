b2495b5e9bebdd574f5d4f2a4f53f7d5eb50c602
Young Seo Kim <youngseokim7@gmail.com>
2026-03-15 15:14:36 -0400

    implement chunk debugger

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "../include/chunk.h"
+#include "../include/debug.h"
 #include <stdio.h>
 
 int main(int argc, char **argv) {
   Chunk chunk;
   init_chunk(&chunk);
   write_chunk(&chunk, OP_RETURN);
+  disassemble_chunk(&chunk, "TEST CHUNK");
   free_chunk(&chunk);
 }
