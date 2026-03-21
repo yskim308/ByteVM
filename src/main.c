3a981d17c12e9560be95c0b19ff8ea41a8929d8c
Young Seo Kim <youngseokim7@gmail.com>
2026-03-20 22:04:28 -0400

    testing of reverse polish notation evaluatio

--- src/main.c
+++ src/main.c
@@ -13,11 +13,23 @@ int main(int argc, char **argv) {
   int constant_idx = add_constant(&chunk, 1.2);
   write_chunk(&chunk, OP_CONSTANT, 123);
   write_chunk(&chunk, constant_idx, 123);
+
+  constant_idx = add_constant(&chunk, 3.4);
+  write_chunk(&chunk, OP_CONSTANT, 123);
+  write_chunk(&chunk, constant_idx, 123);
+
+  write_chunk(&chunk, OP_ADD, 123);
+
+  constant_idx = add_constant(&chunk, 5.6);
+  write_chunk(&chunk, OP_CONSTANT, 123);
+  write_chunk(&chunk, constant_idx, 123);
+
+  write_chunk(&chunk, OP_DIVIDE, 123);
   write_chunk(&chunk, OP_NEGATE, 123);
 
   write_chunk(&chunk, OP_RETURN, 123);
 
-  disassemble_chunk(&chunk, "TEST CHUNK");
+  // disassemble_chunk(&chunk, "TEST CHUNK");
   interpret(&chunk);
 
   free_VM();
