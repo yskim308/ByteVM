be737cfebea2c746a048f593ddc7f40f1a87983f
Young Seo Kim <youngseokim7@gmail.com>
2026-03-23 16:51:19 -0400

    semicolon bugs

--- src/main.c
+++ src/main.c
@@ -65,9 +65,9 @@ int main(int argc, char **argv) {
   if (argc == 1) {
     repl();
   } else if (argc == 2) {
-    run_file(argv[1])
+    run_file(argv[1]);
   } else {
-    fprintf(stderr, "Usage: clox [path]")
+    fprintf(stderr, "Usage: clox [path]");
   }
 
   free_VM();
