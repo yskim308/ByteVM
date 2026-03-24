c32aeb955aa099bc0b0b1c63a19ca08b56c06a78
Young Seo Kim <youngseokim7@gmail.com>
2026-03-24 14:51:12 -0400

    compiler frontend

--- src/compiler.c
+++ src/compiler.c
@@ -1,11 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/common.h"
 #include "../include/compiler.h"
 #include "../include/scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+  bool had_error;
+  bool panic_mode;
+} Parser;
+
+Parser parser;
+
+static void error_at(Token *token, const char *message) {
+  parser.panic_mode = true;
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // nothing
+  } else {
+    fprintf(stderr, "at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.had_error = true;
+}
+
+static void error(const char *message) { error_at(&parser.previous, message); }
+
+static void error_at_current(const char *message) {
+  error_at(&parser.current, message);
+}
+
+static void advance() {
+  parser.previous = parser.current;
+
+  for (;;) {
+    parser.current = scan_token();
+    if (parser.current.type != TOKEN_ERROR)
+      break;
+
+    error_at_current(parser.current.start);
+  }
+}
+
+static void consume(TokenType type, const char *message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  error_at_current(message);
+}
+
 bool compile(const char *source, Chunk *chunk) {
   init_scanner(source);
+
+  parser.had_error = false;
+  parser.panic_mode = false;
+
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
