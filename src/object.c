5b0b3db77c7434ecca29c1004582b86c8540cd56
Young Seo Kim <youngseokim7@gmail.com>
2026-03-31 00:44:58 -0400

    make strings own their hash, and hash on allocate

--- src/object.c
+++ src/object.c
@@ -20,18 +20,30 @@ static Obj *allocate_object(size_t size, ObjType type) {
   return object;
 }
 
-static ObjString *allocate_string(char *chars, int length) {
+static ObjString *allocate_string(char *chars, int length, uint32_t hash) {
   ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
   string->length = length;
   string->chars = chars;
+  string->hash = hash;
   return string;
 }
 
+static uint32_t hash_string(const char *key, int length) {
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < length; i++) {
+    hash ^= (uint8_t)key[i];
+    hash *= 16777619;
+  }
+  return hash;
+}
+
 ObjString *take_string(char *chars, int length) {
-  return allocate_string(chars, length);
+  uint32_t hash = hash_string(chars, length);
+  return allocate_string(chars, length, hash);
 }
 
 ObjString *copy_string(const char *chars, int length) {
+  uint32_t hash = hash_string(chars, length);
   char *heap_chars = ALLOCATE(char, length + 1);
   memcpy(heap_chars, chars, length);
   heap_chars[length] = '\0';
