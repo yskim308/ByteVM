ee52585f0c0ef8f2bbf7ff85352ad69e719736d9
Young Seo Kim <youngseokim7@gmail.com>
2026-04-03 22:45:00 +0900

    fix bugs
    you absolute DUMB AHH... spent 2 hours with lldb for this bullshit god
    damn it

--- src/table.c
+++ src/table.c
@@ -57,14 +57,14 @@ bool table_get(Table *table, ObjString *key, Value *value) {
 static void adjust_capacity(Table *table, int capacity) {
   Entry *entries = ALLOCATE(Entry, capacity);
   for (int i = 0; i < capacity; ++i) {
-    entries->key = NULL;
-    entries->value = NIL_VAL;
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
   }
 
   table->count = 0;
   for (int i = 0; i < table->capacity; ++i) {
     Entry *entry = &table->entries[i];
-    if (entry == NULL)
+    if (entry->key == NULL)
       continue;
 
     Entry *dest = find_entry(entries, capacity, entry->key);
@@ -79,7 +79,7 @@ static void adjust_capacity(Table *table, int capacity) {
 }
 
 bool table_set(Table *table, ObjString *key, Value value) {
-  if (table->capacity + 1 > table->capacity * TABLE_MAX_LOAD) {
+  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
     int capacity = GROW_CAPACITY(table->capacity);
     adjust_capacity(table, capacity);
   }
@@ -132,7 +132,7 @@ ObjString *table_find_string(Table *table, const char *chars, int length,
       if (IS_NIL(entry->value))
         return NULL;
     } else if (entry->key->length == length && entry->key->hash == hash &&
-               memcmp(entry->key->chars, chars, length))
+               memcmp(entry->key->chars, chars, length) == 0)
       return entry->key;
 
     index = (index + 1) % table->capacity;
