e6d24cbc2ff0fd5a6876785d227d1a37984f7f9d
Young Seo Kim <youngseokim7@gmail.com>
2026-04-10 16:00:42 +0900

    support for 'for' loops

--- src/compiler.c
+++ src/compiler.c
@@ -10,6 +10,7 @@
 #include "scanner.h"
 #include "table.h"
 #include "value.h"
+#include "vm.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -627,6 +628,50 @@ static void while_statement() {
   emit_byte(OP_POP);
 }
 
+static void for_statement() {
+  begin_scope();
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+  if (match(TOKEN_SEMICOLON)) {
+    // nothing
+  } else if (match(TOKEN_VAR)) {
+    var_declaration(false);
+  } else {
+    expression_statement();
+  }
+
+  int loop_start = current_chunk()->count;
+  int exit_jump = -1;
+  if (!match(TOKEN_SEMICOLON)) {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expected ';' after loop condition.");
+
+    exit_jump = emit_jump(OP_JUMP_IF_FALSE);
+    emit_byte(OP_POP);
+  }
+
+
+  if (!match(TOKEN_RIGHT_PAREN)) {
+    int body_jump = emit_jump(OP_JUMP);
+    int increment_start = current_chunk()->count;
+    expression();
+    emit_byte(OP_POP);
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    emit_loop(loop_start);
+    loop_start = increment_start;
+    patch_jump(body_jump);
+  }
+
+  statement();
+  emit_loop(loop_start);
+  if (exit_jump != -1) {
+    patch_jump(exit_jump);
+    emit_byte(OP_POP);
+  }
+
+  end_scope();
+}
+
 static void synchronize() {
   parser.panic_mode = false;
 
@@ -666,6 +711,8 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     print_statement();
+  } else if (match(TOKEN_FOR)) {
+    for_statement();
   } else if (match(TOKEN_WHILE)) {
     while_statement();
   } else if (match(TOKEN_IF)) {
